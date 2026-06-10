e2c54f8e3ef14756f31f01bc9896dad4ecc1b0fe
shilkibadal <146571326+shilkibadal@users.noreply.github.com>
2026-06-10 23:52:43 +0530

    Time: 1 ms (99.93%), Space: 46.4 MB (56.9%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,119 @@
+class Solution 
+{
+    public boolean canConstruct(String ransomNote, String magazine) 
+    {
+        int[] count = new int[26];
+
+        // Count letters in magazine
+        for (char c : magazine.toCharArray())
+         {
+            count[c - 'a']++;
+         }
+
+        // Use letters for ransomNote
+        for (char c : ransomNote.toCharArray()) {
+            count[c - 'a']--;
+            if (count[c - 'a'] < 0)
+             {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
